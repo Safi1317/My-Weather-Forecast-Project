6a52784247b7d3339dcee6c3a2d4fe14acb81817
Safia Ali <ali.swaphiyah@gmail.com>
2023-01-23 15:24:32 -0600

    .

--- script.js
+++ script.js
@@ -4,24 +4,21 @@ const cityName = document.getElementById("cityName");
 const temperatureEL = document.getElementById("temperature");
 const Wind = document.getElementById("wind");
 const humidityEL = document.getElementById("humidity");
-var { lat } = location;
-var { lon } = location;
-var city;
 var search;
 function searchWeather() {
   search = document.getElementById("search").value;
-  city = location.name;
-  var { lat } = location;
-  var { lon } = location;
-  var city = location.name;
+  // city = location.name;
+  // var city = location.name;
   fetch(
     `https://api.openweathermap.org/geo/1.0/direct?q=${search},&appid=${apiKey}`
   )
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       fetch(
-        `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${apiKey}`
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${data.index[0].lat}&lon=${data.index[0].lon}&exclude=hourly,daily&appid=${apiKey}`
         // `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${apiKey}`
+        // `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}`
       )
         .then((response) => response.json())
         .then((data) => {
@@ -30,9 +27,10 @@ function searchWeather() {
           //     data.current.temperature;
           //   }
         });
-      console.log(data);
+
       //   for (i = 0; i < 5; i++) {
       //     data.current.temperature;}
+      // });
     });
 }
 // `https://api.openweathermap.org/data/3.0/onecall?lat=${data["current"][0]["lat"]}&lon=${data["current"][1]["lon"]}&exclude=hourly,daily&appid=${apiKey}`
