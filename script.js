a7dec2f88714633ab1ab3685ccbde4b7929a44bf
Safia Ali <ali.swaphiyah@gmail.com>
2023-01-22 21:23:08 -0600

    fixed fetch

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let apiKey = "be1b5ded40845e0479d01eedd998ce1e";
-// let weatherbase = "http://api.openweathermap.org/geo/1.0/direct?q=";
-let searchmethod = "$q=";
-function searchweather() {
-  fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q={City name},&appid={apikey}"
-  ).then(
-    (response) => response.json
-    //   console.log(result)
-  );
-  console.log(response);
-}
 const searchbtn = document.getElementById("searchbtn");
-searchbtn.addEventListener("click", searchweather);
+// const cityName = document.getElementById("cityName");
+const temperatureEL = document.getElementById("temperature");
+const Wind = document.getElementById("wind");
+const humidityEL = document.getElementById("humidity");
+var city;
+
+function searchWeather() {
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},&appid=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      //   for (i = 0; i < 5; i++) {
+      //     data.current.temperature;}
+    });
+}
+
+searchbtn.addEventListener("click", searchWeather);
