9973e38ca5c3af29cd344b94694820b73f8c919f
Safia Ali <ali.swaphiyah@gmail.com>
2023-01-22 23:33:05 -0600

    .

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 let apiKey = "be1b5ded40845e0479d01eedd998ce1e";
 const searchbtn = document.getElementById("searchbtn");
-// const cityName = document.getElementById("cityName");
+const cityName = document.getElementById("cityName");
 const temperatureEL = document.getElementById("temperature");
 const Wind = document.getElementById("wind");
 const humidityEL = document.getElementById("humidity");
-var city;
-
+var search;
 function searchWeather() {
+  search = document.getElementById("search").value;
   fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},&appid=${apiKey}`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${search},&appid=${apiKey}`
   )
     .then((response) => response.json())
     .then((data) => {
+      fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${data["current"][0]["lat"]}&lon=${data["current"][1]["lon"]}&exclude=hourly,daily&appid=${apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.current.lat);
+          //   for (i = 0; i < 5; i++) {
+          //     data.current.temperature;
+          //   }
+        });
       console.log(data);
       //   for (i = 0; i < 5; i++) {
       //     data.current.temperature;}
