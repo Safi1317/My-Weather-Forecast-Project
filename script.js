d2fdd044255b751eef09c6d8f24554ee19b60d75
Safia Ali <ali.swaphiyah@gmail.com>
2023-01-22 20:12:46 -0600

    added eventlistener

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let apiKey = "be1b5ded40845e0479d01eedd998ce1e";
 let searchmethod = "$q=";
 function searchweather() {
   fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q={Newyork},&appid={apikey}"
+    "http://api.openweathermap.org/geo/1.0/direct?q={City name},&appid={apikey}"
   ).then(
     (response) => response.json
     //   console.log(result)
   );
   console.log(response);
 }
+const searchbtn = document.getElementById("searchbtn");
+searchbtn.addEventListener("click", searchweather);
