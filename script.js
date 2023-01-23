ac11218c69aa1a61b86ccdd893291854a7928a05
Safia Ali <ali.swaphiyah@gmail.com>
2023-01-22 20:04:26 -0600

    first draft

---
+++ script.js
@@ -0,0 +1,12 @@
+let apiKey = "be1b5ded40845e0479d01eedd998ce1e";
+// let weatherbase = "http://api.openweathermap.org/geo/1.0/direct?q=";
+let searchmethod = "$q=";
+function searchweather() {
+  fetch(
+    "http://api.openweathermap.org/geo/1.0/direct?q={Newyork},&appid={apikey}"
+  ).then(
+    (response) => response.json
+    //   console.log(result)
+  );
+  console.log(response);
+}
